b68b4429dea8fa37673b1d97b3e926eef7cad237
Wilson <wchampi86@gmail.com>
2015-10-01 14:26:44 -0500

    add changes

--- Ftp.php
+++ Ftp.php
@@ -10,6 +10,8 @@ class ZendR_Ftp
     private $_pasv = true;
     private $_curl = true;
     private $_ftp_stream = null;
+    private $_errorMessage = "";
+    private $_errorCode = null;
 
     public function __construct($server, $port, $user, $password, $pasv = true, $curl = false)
     {
@@ -25,14 +27,38 @@ class ZendR_Ftp
     {
         if ($this->_curl) {
             $this->_ftp_stream = curl_init();
+            if (!$this->_ftp_stream) {
+                $this->_errorMessage = "Invalid Curl Init to $this->_server:$this->_port" ;
+                $this->_errorCode = 101;
+            }
         } else {
             $this->_ftp_stream = ftp_connect($this->_server, $this->_port);
-            ftp_login($this->_ftp_stream, $this->_user, $this->_password);
-            ftp_pasv($this->_ftp_stream, $this->_pasv);
+            if ($this->_ftp_stream) {
+                if (@ftp_login($this->_ftp_stream, $this->_user, $this->_password)) {
+                    if (!ftp_pasv($this->_ftp_stream, $this->_pasv)) {
+                        $this->_errorMessage = "Invalid Turns passive mode ($this->_pasv) to $this->_server:$this->_port. Review Config of Server FTP." ;
+                        $this->_errorCode = 103;
+			return false;
+                    }
+                } else {
+                    $this->_errorMessage = "Invalid Login to $this->_server:$this->_port. Review credentials." ;
+                    $this->_errorCode = 102;
+		    return false;
+                }
+            } else {
+                $this->_errorMessage = "Invalid Connection to '$this->_server:$this->_port'. Review FIREWALL or HOST." ;
+                $this->_errorCode = 101;
+		return false;
+            }
         }  
         return $this->_ftp_stream;
     }
     
+    public function getErrorMessage()
+    {
+        return $this->_errorMessage;
+    }
+        
     public function uploadFile($localFile, $remoteFile)
     {
         if ($this->_ftp_stream) {
@@ -49,9 +75,18 @@ class ZendR_Ftp
                 $error_no = curl_errno($this->_ftp_stream);
                 if ($error_no == 0) {
                     return true;
+                } else {
+                    $this->_errorMessage = curl_error($this->_ftp_stream);
+                    $this->_errorCode = curl_errno($this->_ftp_stream);
                 }
             } else {
-                return ftp_put($this->_ftp_stream, $remoteFile, $localFile, FTP_BINARY);
+                $upload = ftp_put($this->_ftp_stream, $remoteFile, $localFile, FTP_BINARY);
+                if (!$upload) {
+                    $this->_errorMessage = "Upload failed $localFile > $remoteFile FTP_BINARY(" . FTP_BINARY . ")  to $this->_server:$this->_port" ;
+                    $this->_errorCode = 101;
+                }
+                
+                return $upload;
             }    
         }
         return false;
