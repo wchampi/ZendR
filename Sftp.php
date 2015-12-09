c730729aa80ec6025d16b59034c751defc7c445a
wilson <wilson.champi@bongous.com>
2015-12-09 08:59:49 -0500

    Sftp

--- Sftp.php
+++ Sftp.php
@@ -9,6 +9,7 @@ class ZendR_Sftp
     private $_password = '';
     private $_sftp = true;
     private $_connection = null;
+    private $_errorMessage = "";
 
     public function __construct($server, $port, $user, $password)
     {
@@ -22,15 +23,21 @@ class ZendR_Sftp
     {
         $this->_connection = ssh2_connect($this->_server, $this->_port);
 
-        if (!$this->_connection)
-            throw new Exception("Could not connect to $this->_server on port $this->_port.");
+        if (!$this->_connection) {
+            $this->_errorMessage = "Could not connect to $this->_server on port $this->_port.";
+            return false;
+        }
         
-        if (!ssh2_auth_password($this->_connection, $this->_user, $this->_password))
-            throw new Exception("Could not authenticate");
+        if (!ssh2_auth_password($this->_connection, $this->_user, $this->_password)) {
+            $this->_errorMessage = "Could not authenticate";
+            return false;
+        }
         
         $this->_sftp = ssh2_sftp($this->_connection);
-        if (!$this->_sftp)
-            throw new Exception("Could not initialize SFTP subsystem.");
+        if (!$this->_sftp) {
+            $this->_errorMessage = "Could not initialize SFTP subsystem.";
+            return false;
+        }
         
         return true;
     }
@@ -40,19 +47,30 @@ class ZendR_Sftp
         $sftp = $this->_sftp;
         $stream = fopen("ssh2.sftp://$sftp$remoteFile", 'w');
 
-        if (! $stream)
-            throw new Exception("Could not open file: $remoteFile");
+        if (! $stream) {
+            $this->_errorMessage = "Could not open file: $remoteFile";
+            return false;
+        }
 
         $data_to_send = file_get_contents($localFile);
-        if ($data_to_send === false)
-            throw new Exception("Could not open local file: $localFile.");
+        if ($data_to_send === false) {
+            $this->_errorMessage = "Could not open local file: $localFile.";
+            return false;
+        }
 
-        if (fwrite($stream, $data_to_send) === false)
-            throw new Exception("Could not send data from file: $localFile.");
+        if (fwrite($stream, $data_to_send) === false) {
+            $this->_errorMessage = "Could not send data from file: $localFile.";
+            return false;
+        }
 
         fclose($stream);
         
         return true;
+    }
+    
+    public function getErrorMessage()
+    {
+        return $this->_errorMessage;
     }
     
     public function close()
